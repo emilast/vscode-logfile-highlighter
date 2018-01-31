43e5c0888260c41a2bf53cc391d13939294d11bd
HaaLeo <HanischLeo@freenet.de>
2018-01-31 16:43:08 +0100

    Set up integrationtest environment with `jasmine`

--- test/testRunner.ts
+++ test/testRunner.ts
@@ -1,9 +1,11 @@
 import jasmine = require('jasmine');
-import util = require('util');
+import * as path from 'path';
+import * as util from 'util';
 
 class TestRunner {
     public  run(configPath: string, clb: (error: Error) => void): void {
         const runner = new jasmine(null);
+        runner.projectBaseDir = path.dirname(configPath);
         runner.configureDefaultReporter({
             print() {
                 const line = util.format.apply(util.format, arguments);
@@ -37,8 +39,11 @@ if (!module.parent) {
         "node testRunner.js -c ./myConfigPath/jasmine.json"');
         process.exit(1);
     } else {
-        const configPath = process.argv[process.argv.indexOf('-c') + 1];
-        const i = process.cwd();
+        const j = process.argv;
+        let configPath = process.argv[process.argv.indexOf('-c') + 1];
+        if (!path.isAbsolute(configPath)) {
+            configPath = path.normalize(path.dirname(process.argv[1]) + '../../../') + configPath;
+        }
         const runner = new TestRunner();
         runner.run(configPath, (error) => {
             if (error) {
@@ -49,3 +54,5 @@ if (!module.parent) {
         });
     }
 }
+
+export = TestRunner;
