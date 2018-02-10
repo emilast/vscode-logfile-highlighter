def5f079680e2dc4ff1299109b4d198ce22fc8fb
HaaLeo <HanischLeo@freenet.de>
2018-02-10 16:26:05 +0100

    #33 Enables custom log level: * A name * a regexp pattern.

--- src/CustomLogLevel.ts
+++ src/CustomLogLevel.ts
@@ -5,34 +5,56 @@ import * as vscode from 'vscode';
 class CustomLogLevel {
     public readonly value: string;
     public readonly color: string;
-    public readonly regex: string;
+    public readonly regexes: RegExp[];
     public readonly decoration: vscode.TextEditorDecorationType;
 
     public constructor(value: string, color: string) {
         this.value = value;
         this.color = color;
-        this.regex = this.createRegex(value);
+        this.regexes = this.createRegex(value);
         this.decoration = vscode.window.createTextEditorDecorationType({
             color: this.color,
             rangeBehavior: vscode.DecorationRangeBehavior.ClosedClosed
         });
     }
 
-    private createRegex(logLevelValue: string): string {
-        let result: string;
+    private createRegex(logLevelValue: string): RegExp[] {
+        const result: RegExp[] = [];
 
         // Check if the log level value is a "simple" string or not.
         if (!/\w+/g.test(logLevelValue)) {
 
             // log level is already regex.
-            result = logLevelValue;
+            try {
+                result.push(new RegExp(logLevelValue, 'g'));
+            } catch (err) {
+                vscode.window.showErrorMessage('Regex of custom log level is invalid. Error: ' + err);
+            }
         } else {
 
             // Log level consits only of "simple" characters -> build regex.
-            const firstVariant = '(?i)\\b(' + logLevelValue + ')\\:|' +
-                '(?-i)\\b(' + logLevelValue.toUpperCase() + '|' + logLevelValue + ')\\b';
-            const secondVariant = '(?i)\\[(' + logLevelValue + ')\\]';
-            result = '(' + firstVariant + ')|(' + secondVariant + ')';
+            const first = new RegExp('\\b(?!\\[)(' + logLevelValue.toUpperCase() +
+                '|' + logLevelValue + ')(?!\\]|\\:)\\b', 'g');
+            const second = new RegExp('\\[(' + logLevelValue + ')\\]|\\b(' + logLevelValue + ')\\:', 'ig');
+
+            result.push(first, second);
+
+            //// Todo remove
+            // logLevelValue = 'HelloWorld';
+            // const firstVariant = '(?i)\\b(' + logLevelValue + ')\\:|' +
+            //     '(?-i)\\b(' + logLevelValue.toUpperCase() + '|' + logLevelValue + ')\\b';
+            // const secondVariant = '(?i)\\[(' + logLevelValue + ')\\]';
+            // '(' + first.source + '|' + second.source + ')|(' + third.source + ')'
+
+            // let testResult = result[0].test('HelloWorld');
+            // testResult = result[0].test('helloworld:');
+            // testResult = result[0].test('Helloworld'.toUpperCase());
+            // testResult = result[0].test('[helloworld]');
+
+            // testResult = result[1].test('HelloWorld');
+            // testResult = result[1].test('helloworld:');
+            // testResult = result[1].test('Helloworld'.toUpperCase());
+            // testResult = result[1].test('[helloworld]');
         }
 
         return result;
--- src/LogLevelColorizer.ts
+++ src/LogLevelColorizer.ts
@@ -7,13 +7,17 @@ class LogLevelColorizer {
 
     private _customLogLevels: CustomLogLevel[];
 
+    // Indicates in which file which log level highlights which ranges.
+    private _cache: Map<vscode.Uri, Map<CustomLogLevel, vscode.Range[]>>;
+
     public constructor() {
         this._customLogLevels = [];
+        this._cache = new Map<vscode.Uri, Map<CustomLogLevel, vscode.Range[]>>();
     }
 
     public updateConfiguration(): void {
         const configLogLevels = vscode.workspace.getConfiguration('logFileHighlighter').get(
-                'customLogLevels') as Array<{value: string, color: string}>;
+            'customLogLevels') as Array<{ value: string, color: string }>;
 
         for (const loglevel of this._customLogLevels) {
             loglevel.decoration.dispose();
@@ -29,12 +33,84 @@ class LogLevelColorizer {
     }
 
     public colorfyDocument(changedEvent: vscode.TextDocumentChangeEvent): void {
+        if (this._customLogLevels.length === 0 || changedEvent.contentChanges.length === 0) {
+            return;
+        }
+
         const doc = changedEvent.document;
-        const changes = changedEvent.contentChanges;
+
+        const editors = vscode.window.visibleTextEditors.filter((editor) => {
+            return editor.document.fileName === doc.fileName;
+        });
+
+        const change = changedEvent.contentChanges.sort((a, b) => Math.abs(a.range.start.line - b.range.start.line))[0];
+
+        // Start always at the beginning of the line.
+        const startPos = new vscode.Position(change.range.start.line, 0);
+        const docCache = this._cache.get(doc.uri);
+
+        const contentToEnd: string =
+            doc.getText(new vscode.Range(startPos, doc.lineAt(doc.lineCount - 1).range.end));
+
+        for (const logLevel of this._customLogLevels) {
+            const logLevelCache = docCache.get(logLevel);
+
+            // Remove all ranges from the cache that occur after the changed range (change.range).
+            const logLevelRanges = logLevelCache.filter((range) => {
+                return range.end.isBefore(change.range.start);
+            });
+
+            for (const regex of logLevel.regexes) {
+                let matches = regex.exec(contentToEnd);
+
+                while (matches) {
+                    const start = doc.positionAt(doc.offsetAt(startPos) + matches.index);
+                    const end = start.translate(0, matches[0].length);
+                    logLevelRanges.push(new vscode.Range(start, end));
+
+                    matches = regex.exec(contentToEnd);
+                }
+            }
+
+            // Update cache and set decorations.
+            docCache.set(logLevel, logLevelRanges);
+            editors[0].setDecorations(logLevel.decoration, logLevelRanges);
+        }
+
+        this._cache.set(doc.uri, docCache);
     }
 
-    public colorfyEditors(editors: vscode.TextEditor[]): void {
+    public colorfyEditors(editors: vscode.TextEditor[], changes?: vscode.TextDocumentContentChangeEvent[]): void {
+        if (editors.length >= 1) {
+            for (const editor of editors) {
+                const content = editor.document.getText();
 
+                const docRanges = new Map<CustomLogLevel, vscode.Range[]>();
+                for (const logLevel of this._customLogLevels) {
+                    editor.setDecorations(logLevel.decoration, []);
+                    const logLevelRanges = [];
+
+                    for (const regex of logLevel.regexes) {
+                        let matches = regex.exec(content);
+
+                        while (matches) {
+                            const start = editor.document.positionAt(matches.index);
+                            const end = start.translate(0, matches[0].length);
+
+                            logLevelRanges.push(new vscode.Range(start, end));
+
+                            matches = regex.exec(content);
+                        }
+                    }
+
+                    // Update cache and set decorations.
+                    editor.setDecorations(logLevel.decoration, logLevelRanges);
+                    docRanges.set(logLevel, logLevelRanges);
+                }
+
+                this._cache.set(editor.document.uri, docRanges);
+            }
+        }
     }
 }
 
