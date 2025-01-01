8eabb44237fcd2f7212d68d15c9991844bdc9ebf
Emil Åström <emil_astrom@hotmail.com>
2025-01-01 21:30:43 +0100

    Reuse regular expressions for timestamps without dates

--- src/TimestampParsers/TimeFormatParser.ts
+++ src/TimestampParsers/TimeFormatParser.ts
@@ -1,8 +1,9 @@
 'use strict';
+import { RegExpParts } from "./RegExpParts";
 import { TimestampFormatParser, TimestampMatch } from "./TimestampParser";
 
 export class TimeFormatParser implements TimestampFormatParser {
-    private pattern = new RegExp('\\d{2}:\\d{2}(?::\\d{2}(?:[.,]\\d{3}(?<microseconds>\\d{3})?)?)?(?:Z| ?[+-]\\d{2}:\\d{2})?\\b');
+    private pattern = new RegExp(`${RegExpParts.Time}${RegExpParts.Microseconds}${RegExpParts.TimeZone}`);
 
     parse(text: string): TimestampMatch | null {
         const match = this.pattern.exec(text);
